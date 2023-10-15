8279ca999946a2bc77e00f36d0d03fc1f6b514c5
Ryan Xu <xueyix@uw.edu>
2023-10-15 15:15:01 -0700

    can produc graph

--- test1.py
+++ test1.py
@@ -1,15 +1,31 @@
 import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 # Reading the CSV file directly
-df = pd.read_csv("c:\\Users\\20879\\Desktop\\garbage2\\test\\test.csv", skiprows=3)
+try :
+    # Specify data types for multiple columns
+    dtype_dict = {
+        'Impedance Magnitude (Ohms)': float,
+        'Impedance Phase Degrees (I)': float,
+        'Admittance Magnitude (S)': float,
+        'Capacitance Magnitude (F)' : float
+    # Add more columns as needed
+    }
+    columns_to_read = ['Point Number', 'Admittance Magnitude (S)']
+    df = pd.read_csv("C:\\test2.csv", skiprows=3, dtype=dtype_dict, encoding='utf-8', na_values=['NaN', 'N/A', 'NA', 'nan'], usecols=columns_to_read)
 
-
+    print(df.head())
+except Exception as e:
+    print(f"Error: {e}")
 
 # Extracting Time and Capacitance columns
-time = df['Time']
-capacitance = df['Capacitance Magnitude (F)']
+time = df['Point Number']
+capacitance = df['Admittance Magnitude (S)']
+#TODO: need to adjust data layout in csv file
 
 
 # Convert time to minutes for plotting
@@ -22,7 +38,7 @@ def time_to_minutes(t):
     else:
         return parts[0] / 60
 
-minutes = [time_to_minutes(t) for t in time]
+minutes = [time_to_minutes(t) for t in time] #TODO: need to update this
 
 # Adjust time data to start from the first data point
 first_time = minutes[0]
@@ -31,7 +47,7 @@ minutes = [m - first_time for m in minutes]
 # Convert Capacitance from F to pF
 capacitance_pf = [c * 1e12 for c in capacitance]  # 1 F = 1e12 pF
 
-# Plotting the Data
+#Plotting the Data
 plt.figure(figsize=(10, 6))
 plt.plot(minutes, capacitance_pf, marker='o', linestyle='-')
 plt.title("Capacitance Magnitude vs Time")
@@ -41,4 +57,4 @@ plt.xlim(0, 2)  # Set x-axis limits to 0-2 minutes
 plt.ylim(0, 100)  # Set y-axis limits to 0-100 pF
 plt.grid(True)
 plt.tight_layout()  # Adjusts the plot layout
-plt.show()
+plt.show(block = True)
